174644df301db8e636e69d91ecb8babfc188e8d0
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-03-04 17:31:21 +0000

    added template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Joyce Nkengbeza
+// Created on: Feb 2025
 // This file contains the JS functions for index.html
+function myButtonClicked () {
+  alert('Hello, World!')
+}
